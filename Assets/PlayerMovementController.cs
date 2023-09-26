44819ec6bc79e38953f825e99c9d06575bd33813
aleyhin <aleyhin1@gmail.com>
2023-09-26 20:23:29 +0300

    Look function is created and binded for playerPrefab

--- Assets/PlayerMovementController.cs
+++ Assets/PlayerMovementController.cs
@@ -5,8 +5,37 @@ using UnityEngine;
 
 public class PlayerMovementController : NetworkBehaviour
 {
+    private Camera _camera;
+    private Transform _bodyTransform;
+
+    public override void Spawned()
+    {
+        _camera = FindObjectOfType<Camera>();
+        _bodyTransform = GetComponentInChildren<Transform>();
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        Look();
+    }
+
     private void Look()
     {
+        Vector3 rotationVector = new Vector3(0, 0, GetTurnAngle());
+        _bodyTransform.eulerAngles = rotationVector;
+    }
 
+    private float GetTurnAngle()
+    {
+        Vector2 directionVector = (GetMousePositionWorldPoint() - (Vector2)transform.position).normalized;
+        float turnAngle = Mathf.Atan2(directionVector.y, directionVector.x) * Mathf.Rad2Deg;
+        return turnAngle;
+    }
+
+    private Vector2 GetMousePositionWorldPoint()
+    {
+        var inputData = Runner.GetInputForPlayer<PlayerInput>(Object.InputAuthority);
+        Vector2 mousePositionWorldPoint = _camera.ScreenToWorldPoint(inputData.Value.MousePosition);
+        return mousePositionWorldPoint;
     }
 }
